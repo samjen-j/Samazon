4c2f4d3eea729f7c960515995afb76b5087a3872
samjen-j <80285130+samjen-j@users.noreply.github.com>
2022-02-15 23:06:12 -0700

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Samazon.Models;
+using Samazon.Models.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Samazon.Controllers
+{
+    public class HomeController : Controller
+    {
+        private IBookstoreRepository Repo;
+        
+        public HomeController (IBookstoreRepository temp)
+        {
+            Repo = temp;
+        }
+    
+        public IActionResult Index(int pageNum = 1)
+        {
+            int pageSize = 10;
+
+            var x = new BooksViewModel
+            {
+                Books = Repo.Books
+                .OrderBy(b => b.Title)
+                .Skip((pageNum - 1) * pageSize)
+                .Take(pageSize),
+
+                PageInfo = new PageInfo
+                {
+                    TotalNumBooks = Repo.Books.Count(),
+                    BooksPerPage = pageSize,
+                    CurrentPage = pageNum,
+                }
+
+            };
+
+            return View(x);
+        }
+    }
+}
---
+++ Models/EFBookstoreRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Samazon.Models
+{
+    public class EFBookstoreRepository : IBookstoreRepository
+    {
+        private BookstoreContext Context { get; set; }
+        public EFBookstoreRepository (BookstoreContext temp)
+        {
+            Context = temp;
+        }
+        public IQueryable<Book> Books => Context.Books;
+    }
+}
